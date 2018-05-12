f12577597a953e0dc8515ea14279beb74415351e
Tooru Takahashi <tooru.takahashi134@gmail.com>
2018-05-12 13:49:46 +0900

    Added main.go.

---
+++ ch04/s4.3.1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	signals := make(chan os.Signal, 1)
+
+	signal.Notify(signals, syscall.SIGINT)
+
+	fmt.Println("Waiting SIGINT (Ctrl+C)")
+	<-signals
+	fmt.Println("SIGINT arrived")
+}
