ccf0bcc075d84696b1f124614118d775fb33f057
Tooru Takahashi <tooru.takahashi134@gmail.com>
2018-06-02 16:01:22 +0900

    Added main.go.

---
+++ ch09/s9.2.1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func open() {
+	file, err := os.Create("textfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	io.WriteString(file, "New file content\n")
+}
+
+func read() {
+	file, err := os.Open("textfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	fmt.Println("Read file:")
+	io.Copy(os.Stdout, file)
+}
+
+func main() {
+	open()
+	read()
+}
