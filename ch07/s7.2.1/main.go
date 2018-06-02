37f5aa6db723045f1a7aa50645cf59f5c78e6301
Tooru Takahashi <tooru.takahashi134@gmail.com>
2018-06-02 13:49:17 +0900

    Added main.go and main.go.

---
+++ ch07/s7.2.1/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("Server is running at localhost:8888")
+	conn, err := net.ListenPacket("udp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	buffer := make([]byte, 1500)
+
+	for {
+		length, remoteAddress, err := conn.ReadFrom(buffer)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Receive from %v: %v\n",
+			remoteAddress, string(buffer[:length]))
+		_, err = conn.WriteTo([]byte("Hello from Server"),
+			remoteAddress)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
